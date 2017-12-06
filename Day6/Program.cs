04c2f52a7d33b6984bb59dc605f3280cfbe297e3
Jonas Pegerfalk <jonas.pegerfalk@gmail.com>
2017-12-06 21:30:15 +0100

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AocUtil;
+using Xunit;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "14	0	15	12	11	11	3	5	1	6	8	4	9	1	8	4";
+            var seen = new Dictionary<String, int>();
+
+            var d = input.Split(null).Select(int.Parse).ToList();
+            var steps = 0;
+            do
+            {
+                seen[String.Join(",", d)] = steps;
+                var max = d.Max();
+                var indexOfMax = d.IndexOf(max);
+                d[indexOfMax] = 0;
+                for (var i = 1; i <= max; i++)
+                {
+                    d[(indexOfMax + i) % d.Count]++;
+                }
+                steps++;
+            } while (!seen.ContainsKey(String.Join(",", d)));
+            
+            Console.WriteLine(steps);
+            Console.WriteLine(steps-seen[String.Join(",", d)]);
+            Console.ReadKey();
+        }
+    }
+}
