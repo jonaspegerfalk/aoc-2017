27a4829c8df18c0f4e8a7337d65df285e1adbc60
Jonas Pegerfalk <jonas.pegerfalk@gmail.com>
2017-12-09 21:22:37 +0100

    Day 7 and 9 solutions, quickly written but ineffective and ugly

---
+++ Day7/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AocUtil;
+using Xunit;
+
+namespace Day7
+{
+    public class Program
+    {
+
+        static Dictionary<string, ProgramEntry> programs = new Dictionary<string, ProgramEntry>();
+     
+        static void Main(string[] args)
+        {
+            int res = 0;
+            string input = @"..\..\input.txt";
+            var lines = File.ReadLines(input).ToList();
+
+            var i = 0;
+            foreach (var l in lines)
+            {
+                var data = l.Split(' ');
+                var p = new ProgramEntry() { Name = data[0]};
+                p.Weight = int.Parse(data[1].Replace("(", "").Replace(")", ""));
+                programs[data[0]] = p;
+                i++;
+            }
+            foreach (var l in lines)
+            {
+                var data = l.Split();
+                if (data.Length > 2)
+                    for (var j = 3; j<data.Length; j++)
+                {
+                        var a = data[j].Replace(",", "");
+                    programs[a].parent = programs[data[0]];
+                }
+            }
+
+            foreach (var p in programs)
+            {
+                //if (p.Value.parent != null)
+                {
+                    var v = p.Value;
+                    var children = programs.Values.Where(a => a.parent != null && a.parent.Name == v.Name);
+                    var childrenW = children.Select(ch => ch.TotalWeight()).ToList();
+                    if (children.Count() != 0 && childrenW.Max() != childrenW.Min() ) {
+                        Debug.WriteLine(v.Name);
+                    }
+
+                }
+            }
+
+
+            ch("gexwzw");
+
+            Debug.WriteLine(res);
+            Console.ReadKey();
+        }
+
+        static void ch(string name)
+        {
+            var b = programs[name];
+            var children = programs.Values.Where(pr => pr.parent != null && pr.parent.Name == name);
+            var childrenW = children.Select(p => p.TotalWeight()).ToList();
+        }
+
+        public class ProgramEntry
+        {
+            public ProgramEntry parent { get; set; }
+            public string Name { get; set; }
+            public int Weight { get; set; }
+
+            public int TotalWeight()
+            {
+                var children = programs.Values.Where(a => a.parent != null && a.parent.Name == Name);
+                if (children.Count() == 0) return Weight;
+                return Weight + children.Sum(a => a.TotalWeight());
+            }
+        }
+
+
+        [Fact]
+        public static void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Day9/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Day9
+{
+    public class Program
+    {
+        static string CleanData(String data)
+        {
+            var data2 = "";
+            for (var i = 0; i < data.Length; i++)
+            {
+                if (data[i] == '!')
+                {
+                    i++;
+                    continue;
+                }
+                data2 += data[i];
+            }
+            var data22 = "";
+            for (var i = 0; i < data2.Length; i++)
+            {
+                if (data2[i] == '<')
+                {
+                    i = data2.IndexOf('>', i);
+                    continue;
+
+                }
+                data22 += data2[i];
+            }
+            return data22.Replace(",", "");
+        }
+
+        static int CountGroups(string data, int score)
+        {
+            Console.WriteLine($"{score} {new string(' ', score)} {data}");
+            if (data.Length == 2) return score;
+            var open = 0;
+            var res = score;
+            var openTag = 1;
+            for (var i = 1; i < data.Length; i++)
+                {
+                    if (data[i] == '{') open++; else open--;
+                    if (open == 0)
+                {
+                    res += CountGroups(data.Substring(openTag, i-openTag+1), score + 1);
+                    openTag = i+1;
+                }
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            string input = @"..\..\input.txt";
+            var lines = File.ReadLines(input).ToList();
+
+            Console.WriteLine(CountGroups(CleanData(lines[0]), 1));
+            Console.WriteLine(CountGarbage(lines[0]));
+            Console.ReadKey();
+        }
+
+
+        [Fact]
+        public static void Test()
+        {
+            Assert.Equal(1, CountGroups("{}", 1));
+            Assert.Equal(6, CountGroups("{{{}}}",1));
+            Assert.Equal(5, CountGroups(CleanData("{{},{}}"),1));
+            Assert.Equal(16, CountGroups(CleanData("{{{},{},{{}}}}"), 1));
+            Assert.Equal(1, CountGroups(CleanData("{<a>,<a>,<a>,<a>}"),1));
+            Assert.Equal(9, CountGroups(CleanData("{{<!!>},{<!!>},{<!!>},{<!!>}}"), 1));
+            Assert.Equal(9, CountGroups(CleanData("{{<ab>},{<ab>},{<ab>},{<ab>}}"), 1));
+            Assert.Equal(3, CountGroups(CleanData("{{<a!>},{<a!>},{<a!>},{<ab>}}"), 1));
+
+        }
+        [Fact]
+        public static void Test2()
+        {
+            Assert.Equal(0, CountGarbage("<>"));
+            Assert.Equal(17, CountGarbage("<random characters>"));
+            Assert.Equal(3, CountGarbage("<<<<>"));
+            Assert.Equal(0, CountGarbage("<!!!>>"));
+            Assert.Equal(10, CountGarbage("<{o\"i!a,<{i<a>"));
+
+        }
+
+        private static int CountGarbage(string data)
+        {
+            var data2 = "";
+            for (var i = 0; i < data.Length; i++)
+            {
+                if (data[i] == '!')
+                {
+                    i++;
+                    continue;
+
+                }
+                data2 += data[i];
+            }
+            var total = 0;
+            for (var i = 0; i < data2.Length; i++)
+            {
+                if (data2[i] == '<')
+                {
+                    var end  = data2.IndexOf('>', i);
+                    total += (end - i-1);
+                    i = end;
+                    continue;
+
+                }
+            }
+            return total;
+        }
+    }
+}
